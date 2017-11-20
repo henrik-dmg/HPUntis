3283e19fc2f738dbb761071cde251650efbf6c85
Henrik Panhans <henrikpanhans@icloud.com>
2017-11-20 16:39:56 +0100

    Initial Commit

---
+++ SwiftyUntis/SwiftyUntis.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyUntis.h
+//  SwiftyUntis
+//
+//  Created by Henrik Panhans on 20.11.17.
+//  Copyright © 2017 Henrik Panhans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyUntis.
+FOUNDATION_EXPORT double SwiftyUntisVersionNumber;
+
+//! Project version string for SwiftyUntis.
+FOUNDATION_EXPORT const unsigned char SwiftyUntisVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyUntis/PublicHeader.h>
+
+
